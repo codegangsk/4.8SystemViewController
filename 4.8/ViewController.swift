d8c29b8f5bbbb8b847d6c92d86fbdef663e78c52
codegangsk <kjhj0604@gmail.com>
2020-09-23 14:43:45 +0900

    Initial Commit

---
+++ 4.8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4.8
+//
+//  Created by Sophie Kim on 2020/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
