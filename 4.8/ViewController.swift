ccde500e07253edcdc208bafea5636b0bbf45a5b
codegangsk <kjhj0604@gmail.com>
2020-09-23 15:09:07 +0900

    add outlet for image view and create actions for buttons

--- 4.8/ViewController.swift
+++ 4.8/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func safariButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func emailButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
