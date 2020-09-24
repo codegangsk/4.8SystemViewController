099064a7e369c2ed235b07abf555b5a038852031
codegangsk <kjhj0604@gmail.com>
2020-09-24 13:41:29 +0900

    adopt UIImagePickerControllerDelegate and UINavigationControllerDelegate to ViewController class

--- 4.8/ViewController.swift
+++ 4.8/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet var imageView: UIImageView!
     
